4e75cfffa1f2a94396272ec81c40df33f6ea9a2a
Maria Basia <maria.basia1@gmail.com>
2024-07-10 15:36:34 +0300

    Added truncate functionality to recipe description,did some basic stying, added navigation to recipeDetails screen passing recipe id

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,9 +18,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -46,11 +50,13 @@ fun App() {
     NavHost(navController = navController, startDestination = "home") {
 
         composable(route = "home") {
-            HomeScreen()
+            HomeScreen(onNextScreen = { id: Int ->
+                navController.navigate("recipe/$id")
+            })
         }
 
         composable(route = "recipe/{id}") {
-            // RecipeScreen()
+            RecipeScreen()
         }
 
         composable(route = "add") {
@@ -61,54 +67,76 @@ fun App() {
 }
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(onNextScreen: (Int) -> Unit) {
     var recipeArr: Array<Recipes> = arrayOf(
-        Recipes("Test", "", "60", 2,
+        Recipes(1,"Test", "", "60", 2,
             arrayOf("2 tomatoes, 3 potatoes"),
-            "idk"
+            "idkdsffesdfges"
         ),
-        Recipes("Test 2", "", "80", 2,
+        Recipes(2,"Test 2", "", "80", 2,
             arrayOf("3 tomatoes, 2 potatoes"),
-            "idk"
+            "idkidkdsffesdfgeedgfdgidkdsffesdfgeedgfdgidkdsffesdfgeedgfdgidkdsidkdsffesdfgeedgfdgidkdsffesdfgeedgfdgidkdsffesdfgeedgfdgidkds"
         )
     )
 
     Column (modifier = Modifier
         .fillMaxSize()
-        .padding(10.dp)) {
-        Text(text = "Our App")
+        .padding(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Our App",
+            modifier = Modifier
+                .padding(10.dp),
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+            )
         for (recipe in recipeArr) {
-            MakeCard(recipe)
+            MakeCard(recipe, onNextScreen)
             Spacer(modifier = Modifier.height(8.dp))
         }
     }
 
 }
 
+
 @Composable
-fun MakeCard(recipe: Recipes) {
-    return Card {
-        Column (modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp) ) {
+fun RecipeScreen() {
+Box(modifier = Modifier) {
+
+}
+
+
+}
+
+
+@Composable
+fun MakeCard(recipe: Recipes, onNextScreen: (Int) -> Unit) {
+    return Card(onClick = {onNextScreen(recipe.id)}) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+        ) {
 //            Row {
 //
 //            }
             Text(text = recipe.title)
             Text(text = recipe.duration)
-            Text(text = recipe.description)
+            Text(text = truncateDescription(recipe.description))
         }
     }
 }
 
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun truncateDescription(description: String): String {
+    if (description.length >= 50) {
+        return description.substring(0..49) + "..."
+    } else {
+        return description
+    }
 }
+
+
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/recipeapp/Recipes.kt
+++ app/src/main/java/com/example/recipeapp/Recipes.kt
@@ -1,11 +1,12 @@
 package com.example.recipeapp
 
 class Recipes (
-    var title: String = "",
-    var img: String = "",
-    var duration: String = "",
-    var servings: Int = 0,
-    var ingredients: Array<String> = arrayOf(),
-    var description: String =  ""
+    val id: Int,
+    var title: String,
+    var img: String,
+    var duration: String,
+    var servings: Int,
+    var ingredients: Array<String>,
+    var description: String
 ) {
 }
